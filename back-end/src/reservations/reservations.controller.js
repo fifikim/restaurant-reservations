c06799e506763d3702bcebacd6f4ab7753d6ac85
Sophia Kim <mail@fifikim.com>
2021-11-17 11:10:39 -0500

    set up reservations service, controller, router

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,9 +1,13 @@
+const service = require('./reservations.service');
+const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
+
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
+  const reservations = await service.list();
   res.json({
-    data: [],
+    data: reservations,
   });
 }
 
