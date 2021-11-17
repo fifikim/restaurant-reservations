605ba183e179473eb851a5f3496701721eff4a86
Sophia Kim <mail@fifikim.com>
2021-11-17 14:31:34 -0500

    finish US-01 backend

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -9,8 +9,14 @@ const controller = require("./reservations.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router
+  .route("/:reservation_id")
+  .get(controller.read)
+  .all(methodNotAllowed);
+
+router
   .route("/")
   .get(controller.list)
+  .post(controller.create)
   .all(methodNotAllowed);
 
 module.exports = router;
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,9 +1,27 @@
 const knex = require("../db/connection");
 
-function list() {
-  return knex("reservations").select("*");
+function create(reservation) {
+  return knex("reservations")
+    .insert(reservation)
+    .returning("*")
+    .then((createdReservations) => createdReservations[0]);
+}
+
+function list(date) {
+  return knex("reservations")
+    .select("*")
+    .where({ "reservation_date": date })
+    .orderBy("reservation_time");
+}
+
+function read(id) {
+  return knex("reservations")
+    .select("*")
+    .where({ "reservation_id": id });
 }
 
 module.exports = {
+  create,
   list,
+  read,
 };
