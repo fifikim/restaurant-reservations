93904d4ba2f0a0357297bb31d8db0406b36076ad
Sophia Kim <mail@fifikim.com>
2022-01-13 14:19:15 -0500

    convert future time validation to UTC

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -61,9 +61,9 @@ function hasValidDate(req, res, next) {
 // validates that reservation date is in the future
 function hasFutureDate(_req, res, next) {
   const dateTime = res.locals.date + " " + res.locals.time;
-  const reservationDate = new Date.UTC(dateTime);
+  const reservationDate = new Date(dateTime);
   const todaysDate = Date.now();
-  if (todaysDate >= reservationDate) {
+  if (todaysDate.UTC() >= reservationDate.UTC()) {
     return next({
       status: 400,
       message: "Reservation must occur in the future",
