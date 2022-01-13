0e9d41e600229906c1c3e96fa9156d6b6166e2ff
Sophia Kim <mail@fifikim.com>
2022-01-13 10:06:15 -0500

    add comments

--- back-end/src/errors/asyncErrorBoundary.js
+++ back-end/src/errors/asyncErrorBoundary.js
@@ -1,3 +1,11 @@
+/**
+ * Higher-order function used to handle potential errors from 
+ * an asynchronous controller function calling the API.
+ * 
+ * @param delegate: async/await handler or middleware function
+ * @param defaultStatus: optional status code to return if delegate throws an error
+ * @returns Express next() handler or middleware function
+ */
 function asyncErrorBoundary(delegate, defaultStatus) {
   return (request, response, next) => {
     Promise.resolve()
